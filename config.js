844a74f09a4633dcb81016a38b730a4d4ec048b0
quangvinh99er <67144311+quangvinh99er@users.noreply.github.com>
2022-06-01 21:16:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Goodboy send Fuckboy",
+    introTitle: 'Phong bê đê',
+    introDesc: `Trái đất vốn lạ vcc
+    Mà sao em bị bê đê`,
+    btnIntro: 'Goodboy send Fuckboy',
+    title: 'Bê đê chúa đúng hem',
+    desc: 'Goodboy send Fuckboy',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn em lại bị bê đê ',
+    btnReply: 'Goodboy send Fuckboy ',
+    reply: '3d thì 3d mà không 3d thì 3d ',
+    mess: 'Goodboy send Fuckboy',
+    messDesc: 'Hẹn em 12h chơi net',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
